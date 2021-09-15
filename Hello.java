3c3352e08b0fd76f9bf4ca4968089a12e524a9ba
Roman Gamzin <Prom1tey@bk.ru>
2021-09-15 21:59:59 +0300

    gfsgd

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+class Hello{
     public static void main(String args[]) {
-        System.out.println("\nHello World");
+        System.out.println("A\nHello World");
     }
 }
